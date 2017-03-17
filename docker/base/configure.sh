a3998008010d1a56b67acb11d2258914ba60d08b
Thanyaboon Tovorapan <whatineat@gmail.com>
2017-03-17 22:02:59 +0700

    First Commit

---
+++ docker/base/configure.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+if [ -n "$1" ]; then
+   cat >> conf/storm.yaml <<EOF
+storm.zookeeper.servers:
+	- "$1"
+EOF
+fi
+
+if [ -n "$2" ]; then
+   cat >> conf/storm.yaml <<EOF
+nimbus.seeds: ["$2"]
+EOF
+fi   
+
+cat >> conf/storm.yaml <<EOF
+storm.local.dir: "/tmp"
+EOF
+
+cat conf/storm.yaml
